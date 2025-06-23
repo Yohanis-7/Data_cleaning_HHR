a34a91de6ec0b837ea585b32bfed3fcf5542bfd1
Yohanis Alemeshet Asefa <118360056+Yohanis-7@users.noreply.github.com>
2025-06-23 14:26:11 +0300

    Create data_cleaning.R

---
+++ data_cleaning.R
@@ -0,0 +1,9 @@
+
+### set your working directory
+getwd()
+setwd("D:/Data_Science_HHR/Intro-to-R")
+
+## download necessary packages
+
+install.packages("tidyverse",dependencies = T)
+install.packages("readxl")
